3b37f1bf639df3c77973ba543f3ddb3681596729
Dwayne Brown <dwayne.brown@bazaarvoice.com>
2015-11-04 17:00:11 +0000

    Update IR PHP

--- InlineRatings/InlineRatingsPHPExample.php
+++ InlineRatings/InlineRatingsPHPExample.php
@@ -1,34 +1,39 @@
 <?php
+  /*
+  *  This code is provided for demonstration purposes only and is not intended for copy and paste integrations.
+  *  Ce code ne sert qu'à des fins de démonstration et ne devrait pas être integré en copier-coller.
+  */
 
 echo "Inline Ratings with basic PHP";
 
 $product[] = 'Product1'; // Produit par défaut
-$product[] = 'Product1'; // les étoiles n'afficheront qu'une fois par référence produit
-$product[] = 'Product1'; // donc ce script n'affichera qu'ne fois les étoiles par exemple
-$product[] = 'Product1';
+$product[] = 'Product2'; // les étoiles n'afficheront qu'une fois par référence produit
+$product[] = 'Product3';
+$product[] = 'Product4';
 
 echo '<script type="text/javascript" src="//display-stg.ugc.bazaarvoice.com/static/clientname/locale/bvapi.js"></script>';
 
 $i = 0;
 $BVui = '<script>
   $BV.ui( "rr", "inline_ratings", {
-                productIds : [';
+    productIds : [';
 
-                  foreach ($product as $p) {
-                    ++$i;
-                    $BVui .= "\"$p\"";
-                    $BVui .= ( $i != count($product)  ? "," : "");
-                  }
+      foreach ($product as $p) {
+        ++$i;
+        $BVui .= "\"$p\"";
+        $BVui .= ( $i != count($product)  ? "," : "");
+      }
 
-                  $BVui .= '],
-                    containerPrefix : "BVRRInlineRating"
-                  });
-              </script>';
+      $BVui .= '],
+        containerPrefix : "BVRRInlineRating"
+      });
+  </script>';
 
 echo $BVui;
+
 ?>
 
-<div id="BVRRInlineRating-<?php echo $product[0]; ?>" class="inline-rating l-block"></div>
-<div id="BVRRInlineRating-<?php echo $product[1]; ?>" class="inline-rating l-block"></div>
-<div id="BVRRInlineRating-<?php echo $product[2]; ?>" class="inline-rating l-block"></div>
-<div id="BVRRInlineRating-<?php echo $product[3]; ?>" class="inline-rating l-block"></div>
+<div id="BVRRInlineRating-<?php echo $product[0]; ?>" ></div>
+<div id="BVRRInlineRating-<?php echo $product[1]; ?>" ></div>
+<div id="BVRRInlineRating-<?php echo $product[2]; ?>" ></div>
+<div id="BVRRInlineRating-<?php echo $product[3]; ?>" ></div>
